cddd713fbb404fca6e0dd3148648e9a8097adda4
Alex Lane <alane@wellviewhealth.com>
2020-10-21 11:00:35 -0400

    added additional test to test the installment amounts

--- SoftwareEngineerInterview/CSharp/QuadPay.InstallmentsService/QuadPay.InstallmentsService.Test/PaymentPlanServiceTests.cs
+++ SoftwareEngineerInterview/CSharp/QuadPay.InstallmentsService/QuadPay.InstallmentsService.Test/PaymentPlanServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using QuadPay.InstallmentsService.Services;
 using Shouldly;
 using Xunit;
@@ -13,10 +14,42 @@ namespace QuadPay.InstallmentsService.Test
             var paymentPlanService = new PaymentPlanService();
             
             // Act
-            var paymentPlan = paymentPlanService.CreatePaymentPlan(123.45M);
+            var paymentPlan = paymentPlanService.CreatePaymentPlan((decimal)123.45);
 
             // Assert
             paymentPlan.ShouldNotBeNull();
+        }
+        
+        [Theory]
+        [InlineData(123.44, 30.86, 30.86, 30.86, 30.86)]
+        [InlineData(123.45, 30.87, 30.86, 30.86, 30.86)]
+        [InlineData(123.46, 30.87, 30.87, 30.86, 30.86)]
+        [InlineData(123.47, 30.87, 30.87, 30.87, 30.86)]
+        [InlineData(123.48, 30.87, 30.87, 30.87, 30.87)]
+        public void WhenCreatePaymentPlanWithValidOrderAmount_ShouldReturnValidPaymentPlanInstallmentAmounts(
+            decimal amount,
+            decimal firstInstallmentAmount,
+            decimal secondInstallmentAmount,
+            decimal thirdInstallmentAmount,
+            decimal fourthInstallmentAmount
+        )
+        {
+            // Arrange
+            var paymentPlanService = new PaymentPlanService();
+            
+            // Act
+            var paymentPlan = paymentPlanService.CreatePaymentPlan(amount);
+
+            // Assert
+            paymentPlan.ShouldNotBeNull();
+            
+            // Assert installment amounts
+            var installments = paymentPlan.Installments.ToArray();
+            
+            installments.ToArray()[0].Amount.ShouldBe(firstInstallmentAmount);
+            installments.ToArray()[1].Amount.ShouldBe(secondInstallmentAmount);
+            installments.ToArray()[2].Amount.ShouldBe(thirdInstallmentAmount);
+            installments.ToArray()[3].Amount.ShouldBe(fourthInstallmentAmount);
         }
     }
 }
